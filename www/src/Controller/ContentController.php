b5ea2a64d54526a286e1881b143c105e33dd97bc
Denis <d.drozdetsky@faberlic.com>
2023-06-22 18:52:37 +0300

    sign_in sign_out

--- www/src/Controller/ContentController.php
+++ www/src/Controller/ContentController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class ContentController extends AbstractController
 {
     #[Route('/content', name: 'app_content')]
-    public function index(): Response
+    public function index(Security $security): Response
     {
+        /** @var User|null $user */
+        $user = $security->getUser();
         return $this->render('content/index.html.twig', [
+            'username' => $user instanceof User ? $user->getUserIdentifier() : '',
             'controller_name' => 'ContentController',
         ]);
     }
